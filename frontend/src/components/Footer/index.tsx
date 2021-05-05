4fd922d2f131f1a6589466b5ac7105e8a7289836
root <root@debian>
2021-05-04 21:41:07 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+            <p className="text-light">App desenvolvido por <a href="https://github.com/LucasDev13" target="_blank" rel="noreferrer">Lucas pontes - Analista e desenvolvedor de sistemas</a></p>
+            <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+      LinkedIn para contatos: <a href="https://www.linkedin.com/in/pontes-lucas/" target="_blank" rel="noreferrer">@LucasPontes</a></small></p>
+  </div>
+</footer>
+    );
+  }
+  
+  export default Footer;
